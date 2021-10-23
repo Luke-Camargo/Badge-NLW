6398c729bd7c6a6690d503abd814bddb76a31e22
Luke_Camargo <92608977+Luke-Camargo@users.noreply.github.com>
2021-10-23 18:26:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+const LinksSocialMedia = {
+  github: 'Luke-Camargo',
+  instagram: '_luke_camargo_',
+  youtube: 'UCygYOyIP6-OnPPrt2o6lGZg'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(Response => Response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+    })
+}
+getGitHubProfileInfos()
